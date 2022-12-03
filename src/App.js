5607d4a8c6fb661f88e7d64b7015454105a9ad33
PrabhVirdi27 <virdiprabhsingh@gmail.com>
2022-12-03 04:28:11 -0500

    deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="App">
+
+      <h1 className="headinglarge">Problem 3 lab10c Prabh Virdi</h1>
+      <br></br>
+      <h3>How I installed the framework</h3>
+      <div className="steps">
+      <p>Step 1: Went to wwww.nodejs.org.</p>
+      <p>Step 2: Downloaded the node.js installer for windows 64bit recommended version.</p>
+      <p>Step 3: Ran the nodejs installer which was downloaded.</p>
+      <p>Step 4: After the installer was done, clicked on finish.</p>
+      <p>Step 5: Opened command prompt and typed "node -v" to confirm installation and version.</p>
+      <p>Step 6: Node.js frame work is now installed.</p>
+      </div>
+      <br></br>
+      <h3>How I built the page</h3>
+      <div className="steps">
+      <p>Step 1: Created a folder called "Node js apps" on my C drive.</p>
+      <p>Step 2: Used command line and went to the "Node js apps" folder.</p>
+      <p>Step 3: Used the command "npx create-react-app lab10c" to install react app in the folder.</p>
+      <p>Step 4: Opened VS Code and opened the lab10c folder which was created.</p>
+      <p>Step 5: Opened the terminal in VS Code.</p>
+      <p>Step 6: Executed the command "npm start" to see the default page on local computer.</p>
+      <p>Step 7: In VS code, clicked on src inside the lab10c folder.</p>
+      <p>Step 8: Built my page by using the js and css files inside src.</p>
+      </div>
+      <br></br>
+      <h3>Difficulties encountered and how I solved them</h3>
+      <div className="steps">
+      <p>Firstly, I had difficulties with the command line because I was having trouble going into 
+        the right folders needed to run the different commands in. I solved this by checking and double 
+        checking every folder which I was entering through the command line. 
+        <br></br>
+        Also, I had trouble with the commands themselves as they needed to be edited a bit to suit 
+        the app names I was creating along with 
+        how I wanted my app to work in react. I solved this by looking up how the command works before 
+        running it in the command line.
+      </p>
+      <br></br>
+      </div>
+      </body>
   );
 }
 
